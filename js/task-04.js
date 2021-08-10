e308b4af0b235d394d07433502cbb919374c6fc2
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-08-10 22:42:55 +0300

    task 04

---
+++ js/task-04.js
@@ -0,0 +1,27 @@
+// Лічильник складається зі спана і кнопок, які повинні збільшувати і зменшувати
+// значення лічильника на `1`.
+
+// - Створи змінну `counterValue` в якій буде зберігається поточне значення
+//     лічильника.
+// - Створи функції `increment` і `decrement` для збільшення і зменшення значення
+//     лічильника.
+// - Додай слухачі кліків на кнопки, виклики функцій та оновлення інтерфейсу
+
+const counterRef = document.querySelector("#value");
+const divRef = document.querySelector("#counter");
+let counterValue = counterRef.textContent;
+
+const decButton = divRef.firstElementChild;
+const incButton = divRef.lastElementChild;
+
+decButton.addEventListener("click", decValue);
+incButton.addEventListener("click", incValue);
+
+function incValue() {
+  counterValue = Number(counterValue) + 1;
+  counterRef.textContent = counterValue;
+}
+function decValue() {
+  counterValue -= 1;
+  counterRef.textContent = counterValue;
+}
