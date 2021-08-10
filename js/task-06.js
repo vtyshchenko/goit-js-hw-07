18303a82cd51b884f384fade752fe6427c8034c2
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-08-10 23:14:09 +0300

    task 06

---
+++ js/task-06.js
@@ -0,0 +1,21 @@
+// Напиши скрипт, який би при втраті фокуса на інпут, перевіряв його вміст на
+// правильну кількість символів.
+
+// - Скільки символів має бути в інпут, вказується в його атрибуті `data-length`.
+// - Якщо введена відповідна кількість, то `border` інпут стає зеленим,   якщо
+//   неправильне - червоним.
+
+// Для додавання стилів, використовуй CSS-класи `valid` і `invalid`.
+
+const inputRef = document.querySelector("#validation-input");
+const countLetters = Number(inputRef.attributes["data-length"].value);
+
+inputRef.addEventListener("blur", onOutFocus);
+
+function onOutFocus(params) {
+  const clName =
+    params.target.value.length === countLetters ? "valid" : "invalid";
+  inputRef.classList.remove("valid");
+  inputRef.classList.remove("invalid");
+  inputRef.classList.add(clName);
+}
